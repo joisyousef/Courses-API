89c541aac803761cf2cadead906f46c34a9f081d
joisyousef <yousefelsrogy500@gmail.com>
2025-03-12 23:45:32 +0200

    async wrapper

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -1,6 +1,8 @@
 import { validationResult } from "express-validator";
 import { Course } from "../models/course.model.js";
 import httpStatusText from "../utils/httpStatusText.js";
+import asyncWrapper from "../middlewares/asyncWrapper.js";
+
 const getAllCourses = async (req, res) => {
   const query = req.query;
 
@@ -12,25 +14,20 @@ const getAllCourses = async (req, res) => {
 
   res.json({ status: httpStatusText.SUCCESS, data: { courses } });
 };
-const getCourse = async (req, res) => {
-  try {
-    const course = await Course.findById(req.params.id);
-    if (!course) {
-      return res.status(404).json({
-        status: httpStatusText.FAIL,
-        data: { course: "Course Not Found" },
-      });
-    }
-    res.json({ status: "success", data: { course } });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      status: httpStatusText.ERROR,
-      message: err.message,
-      code: 400,
-    });
+const getCourse = asyncWrapper(async (req, res) => {
+  const course = await Course.findById(req.params.id);
+  if (!course) {
+    const error = new Error("Course not found");
+    return next(error);
+    return res.status(404).json({ status: httpStatusText.FAIL, data: null });
   }
-};
+  return res.json({
+    status: httpStatusText.SUCCESS,
+    data: {
+      course,
+    },
+  });
+});
 const addCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from "express";
 import coursesRoute from "./routes/courses.route.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import httpStatusText from "./utils/httpStatusText.js";
 
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
 const url = process.env.MONGO_URL;
@@ -14,9 +15,19 @@ mongoose.connect(url).then(() => {
   console.log("Connected to MongoDB");
 });
 
+app.use(cors());
+
 app.use("/api/courses", coursesRoute);
+
+// Glopal Error Handler
 app.all("*", (req, res, next) => {
   res.status(404).json({ message: "Route not foudnd" });
+});
+// Glopal Middleware Error Handler
+app.use((error, req, res, next) => {
+  res
+    .status(500)
+    .json({ status: httpStatusText.ERROR, message: error.message });
 });
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
